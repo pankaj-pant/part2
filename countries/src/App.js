35ebb4f0b2ba8c2f08a7b2c818cba2a89bd4c510
pankaj-pant <pankajpant89@gmail.com>
2019-12-10 11:49:54 +0200

    2.13, 2.14

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [countries, setCountries] = useState([])
   const [filtered, setFiltered] = useState('')
-  const [weather, setWeather] = useState(null)
+  //const [weather, setWeather] = useState(null)
 
 
   useEffect(() => {
@@ -17,15 +16,39 @@ function App() {
       })
   }, [])
 
+  const Weather = ({ country }) => {
+    const [weather, setWeather] = useState("");
   
-  const fetchData = async () => {
-    const result = await axios(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=5ea4a910ca431288ef44af3146fd1866`,
-      );
-      const weatherCondition = await result.data;
-      setWeather(weatherCondition);
-      console.log('Inside fetchData')
-  };
+    let capital = country.capital;
+  
+    useEffect(() => {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${capital}&units=metric&appid=5ea4a910ca431288ef44af3146fd1866`
+        )
+        .then(response => {
+          const data = response.data;
+          const weatherObject = {
+            temperature: data.main.temp,
+            windDirection: data.wind.deg,
+            windSpeed: data.wind.speed,
+            weatherImage: data.weather[0].icon
+          };
+          setWeather(weatherObject);
+        });
+    }, [capital]);
+
+    return (
+      <div>
+        <h3>Weather in {capital}</h3>
+        <b>temperature:</b> {weather.temperature} Celcius
+        <br />
+        <img src={`http://openweathermap.org/img/wn/${weather.weatherImage}@2x.png`} alt="weatherImage" />
+        <br />
+        <b> wind:</b> {weather.windSpeed} kph direction {weather.windDirection} degrees
+      </div>
+    )
+  }
     
 
   const handleChange = (event) => {
@@ -34,10 +57,11 @@ function App() {
 
   const display = countries.filter(c => c.name.toString().toLowerCase().includes(filtered.toString().toLowerCase()) === true)
 
-  
+  const handleClick = name => {
+    setFiltered(name)
+  }
 
   let text;
-  let city;
   if(filtered === "") {
     text = <p>Search for country</p>
   } else if (display.length > 10) {
@@ -46,9 +70,9 @@ function App() {
     text = display.map(d => 
       <p key={d.alpha3Code}>
         {d.name}
+        <button onClick={() => handleClick(d.name)}>show</button>
       </p>)
   } else {
-    city = display[0].capital;
     text = display.map(d => 
       <div key={d.alpha3Code}>
         <h2>{d.name}</h2>
@@ -58,10 +82,11 @@ function App() {
         <ul>
           {d.languages.map(l => <li key={l.name}>{l.name}</li>)}
         </ul>
-        <img src={d.flag} height='200px' width='400px' />
+        <img src={d.flag} height='200px' width='400px' alt='flag'/>
+        <Weather country={d} />
       </div>
       )
-      
+    
   }
 
   return (
